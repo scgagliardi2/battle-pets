f3196a3f92addcaebd48e8e9f3ac56cc9525136d
Samuel Gagliardi <samuelgagliardi@Samuels-iMac.local>
2020-04-06 14:42:01 -0500

    initiate app

---
+++ ios/Pods/Target Support Files/Pods-BattlePets-BattlePetsTests/Pods-BattlePets-BattlePetsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BattlePets_BattlePetsTests : NSObject
+@end
+@implementation PodsDummy_Pods_BattlePets_BattlePetsTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-BattlePets/Pods-BattlePets-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BattlePets : NSObject
+@end
+@implementation PodsDummy_Pods_BattlePets
+@end
